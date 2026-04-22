5938cb4438399281839aeae83147347bed233042
DANIELKLFL <01033948385pjh@gmail.com>
2026-04-22 16:13:18 +0900

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2RIPbCkPgHt_qKAb1Sw2r-CyY-IroNJk",
+  authDomain: "forklift-market.firebaseapp.com",
+  projectId: "forklift-market",
+  storageBucket: "forklift-market.firebasestorage.app",
+  messagingSenderId: "434473189095",
+  appId: "1:434473189095:web:19269a7ccd304a436e2c42"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
