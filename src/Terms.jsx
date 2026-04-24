a5a434a5d265bbcdb5e302af61a25f7837594948
DANIELKLFL <01033948385pjh@gmail.com>
2026-04-24 15:29:54 +0900

    Create Terms.jsx

---
+++ src/Terms.jsx
@@ -0,0 +1,16 @@
+export default function Terms() {
+  return (
+    <div style={{ padding: 40, color: "#fff" }}>
+      <h1>이용약관</h1>
+
+      <h3>서비스 목적</h3>
+      <p>중고지게차 매물 정보 제공 플랫폼</p>
+
+      <h3>회원 의무</h3>
+      <p>허위매물 등록 금지</p>
+
+      <h3>책임</h3>
+      <p>거래 책임은 판매자와 구매자에게 있습니다.</p>
+    </div>
+  );
+}
